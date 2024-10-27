40118a6cb00165a08719d62d280b0e3f31950ec0
Nolan Liogier <nliogier43@gmail.com>
2024-10-28 00:02:38 +0100

    site_v1.1.2

---
+++ js/popup.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    document.getElementById("popup").style.display = "flex";
+  }
+  
+  function closePopup() {
+    document.getElementById("popup").style.display = "none";
+  }
+  
